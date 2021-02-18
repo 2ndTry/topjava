43c8f4c02e7c3a19d590d7845fda93911ed81cae
Alexey Mirniy <mirnijalex@gmail.com>
2021-02-18 09:03:17 +0300

    3 01 HW2 repositories

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -6,8 +6,6 @@ import java.time.LocalTime;
 
 public class Meal extends AbstractBaseEntity{
 
-    private final Integer userId;
-
     private Integer id;
 
     private final LocalDateTime dateTime;
